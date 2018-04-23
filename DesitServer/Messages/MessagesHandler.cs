dd237dc3f9d80627ecdb94497ebe39a45dea8a87
Franco Llamas <francollamas@desit-sa.com>
2018-04-23 19:25:46 -0300

    Proyecto creado

---
+++ DesitServer/Messages/MessagesHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using WebSocketManager;
+using WebSocketManager.Common;
+
+namespace DesitServer.Messages
+{
+    /**
+     * Mensajes del servidor al cliente
+     */
+    public class MessagesHandler : WebSocketHandler
+    {
+        public MessagesHandler(WebSocketConnectionManager webSocketConnectionManager) : base(webSocketConnectionManager)
+        {
+        }
+
+        /**
+         * Evento que sucede cuando se conecta un Dispositivo
+         */
+        public override async Task OnConnected(WebSocket socket)
+        {
+            await base.OnConnected(socket);
+            var socketId = WebSocketConnectionManager.GetId(socket);
+
+            var message = new Message() {
+                MessageType = MessageType.Text,
+                Data = $"Cliente: {socketId} conectado!"
+            };
+
+            await SendMessageToAllAsync(message);
+        }
+
+        /**
+         * Evento que sucede cuando se desconecta un Dispositivo
+         */
+        public override async Task OnDisconnected(WebSocket socket)
+        {
+            var socketId = WebSocketConnectionManager.GetId(socket);
+            await base.OnDisconnected(socket);
+
+            var message = new Message() {
+                MessageType = MessageType.Text,
+                Data = $"Cliente: {socketId} desconectado."
+            };
+            await SendMessageToAllAsync(message);
+        }
+
+
+        public async Task SendMessage(string socketId, string message)
+        {
+            System.Diagnostics.Debug.WriteLine(message);
+            await InvokeClientMethodToAllAsync("receiveMessage", socketId, message);
+        }
+    }
+}
---
+++ DesitServer/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DesitServer.Messages;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using WebSocketManager;
+
+namespace DesitServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddWebSocketManager();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider sv)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            app.UseFileServer();
+            app.UseStaticFiles();
+            app.UseWebSockets();
+            app.MapWebSocketManager("/messages", sv.GetService<MessagesHandler>());
+        }
+    }
+}

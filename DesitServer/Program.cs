92bd6b80e2585b767497106c7848f446526031e8
Franco Llamas <francollamas@desit-sa.com>
2018-05-31 19:14:25 -0300

    asd

--- DesitServer/Program.cs
+++ DesitServer/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -20,6 +20,7 @@ namespace DesitServer
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseKestrel()
+                .UseUrls("http://0.0.0.0:5000")
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
